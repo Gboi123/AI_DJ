d5e04b470aeaddcac5fc2736a39a8c55c67f595c
Gboi123 <85195842+Gboi123@users.noreply.github.com>
2022-04-12 19:47:02 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 song = ""
-
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function preload(){
     song = loadSound("music.mp3");
 }
@@ -9,6 +12,9 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotPoses);
 }
 function draw() {
     image(video, 0, 0, 600, 500);
@@ -16,4 +22,24 @@ function draw() {
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1)
+}
+
+function modelLoaded() {
+    console.log('Posenet is Initialized');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWrist = " + leftWristX +"leftWristY = " + leftWristY);
+    
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("RightWrist = " + RightWristX +"RightWristY = " + RightWristY);
+    }
+    
 }
